cb3ce91288616d4ec8b9712acf20c053063f6790
OneGraund <vovangaunder@gmail.com>
2023-02-28 12:53:24 +0100

    debugging

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ def check_if_muted():
 
     r,g,b = pyautogui.pixel(MUTE_COORDS[0], MUTE_COORDS[1])
     if r>=237 and g<=20 and b<=20:
+        print('System sounds are muted')
         return True
     else:
+        print(
+            'System sounds are not muted'
+        )
         return False
 
 def unmute_sounds():
